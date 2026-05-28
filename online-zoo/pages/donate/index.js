bbd39720dcb582b62093adb91fd068c5eb962159
Adil240199 <igrok.nomer99@mail.ru>
2026-05-28 12:34:11 +0200

    feat: improve accessibility and fix UI/JS issues
    - fixed hover logic bugs and reduced JS duplication in main and donate pages
    - improved semantic HTML structure
    - fixed broken tags in tablet menu
    - replaced invalid button > a patterns with proper links styled as buttons
    - added keyboard support for burger menu (Enter, Space, Escape)
    - improved aria attributes (aria-controls, aria-expanded)
    - fixed typos in UI text and social/testimonial labels
    - normalized donate link paths
    - removed duplicate stylesheet in donate page

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -3,95 +3,47 @@ let animal = document.querySelectorAll(".figure");
 let hText = document.querySelectorAll(".circle_indicatorBig");
 let dollar = document.querySelectorAll(".dollar");
 
-animal[0].addEventListener("mouseover", function hov() {
-  textAnimal[0].classList.add("hovered");
-  hText[0].classList.add("hovered");
-  dollar[0].classList.add("hovered");
-});
-animal[0].addEventListener("mouseout", function hove() {
-  textAnimal[0].classList.remove("hovered");
-  hText[0].classList.remove("hovered");
-  dollar[0].classList.remove("hovered");
-});
-animal[1].addEventListener("mouseover", function hov() {
-  textAnimal[1].classList.add("hovered");
-  hText[1].classList.add("hovered");
-  dollar[1].classList.add("hovered");
-});
-animal[1].addEventListener("mouseout", function hove() {
-  textAnimal[1].classList.remove("hovered");
-  hText[1].classList.remove("hovered");
-  dollar[1].classList.remove("hovered");
-});
-animal[2].addEventListener("mouseover", function hov() {
-  textAnimal[2].classList.add("hovered");
-  hText[2].classList.add("hovered");
-  dollar[2].classList.add("hovered");
-});
-animal[2].addEventListener("mouseout", function hove() {
-  textAnimal[2].classList.remove("hovered");
-  hText[2].classList.remove("hovered");
-  dollar[2].classList.remove("hovered");
-});
-animal[3].addEventListener("mouseover", function hov() {
-  textAnimal[3].classList.add("hovered");
-  hText[3].classList.add("hovered");
-  dollar[3].classList.add("hovered");
-});
-animal[3].addEventListener("mouseout", function hove() {
-  textAnimal[3].classList.remove("hovered");
-  hText[3].classList.remove("hovered");
-  dollar[3].classList.remove("hovered");
-});
-animal[4].addEventListener("mouseover", function hov() {
-  textAnimal[4].classList.add("hovered");
-  hText[4].classList.add("hovered");
-  dollar[4].classList.add("hovered");
-});
-animal[4].addEventListener("mouseout", function hove() {
-  textAnimal[4].classList.remove("hovered");
-  hText[4].classList.remove("hovered");
-  dollar[4].classList.remove("hovered");
-});
-animal[5].addEventListener("mouseover", function hov() {
-  textAnimal[5].classList.add("hovered");
-  hText[5].classList.add("hovered");
-  dollar[5].classList.add("hovered");
-});
-animal[5].addEventListener("mouseout", function hove() {
-  textAnimal[5].classList.remove("hovered");
-  hText[5].classList.remove("hovered");
-  dollar[5].classList.remove("hovered");
-});
-animal[6].addEventListener("mouseover", function hov() {
-  textAnimal[6].classList.add("hovered");
-  hText[6].classList.add("hovered");
-  dollar[7].classList.add("hovered");
-});
-animal[6].addEventListener("mouseout", function hove() {
-  textAnimal[6].classList.remove("hovered");
-  hText[6].classList.remove("hovered");
-  dollar[6].classList.remove("hovered");
-});
-animal[7].addEventListener("mouseover", function hov() {
-  textAnimal[7].classList.add("hovered");
-  hText[7].classList.add("hovered");
-  dollar[7].classList.add("hovered");
-});
-animal[7].addEventListener("mouseout", function hove() {
-  textAnimal[7].classList.remove("hovered");
-  hText[7].classList.remove("hovered");
-  dollar[7].classList.remove("hovered");
+animal.forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+    textAnimal[index].classList.add("hovered");
+    hText[index].classList.add("hovered");
+    dollar[index].classList.add("hovered");
+  });
+
+  item.addEventListener("mouseout", () => {
+    textAnimal[index].classList.remove("hovered");
+    hText[index].classList.remove("hovered");
+    dollar[index].classList.remove("hovered");
+  });
 });
 
 
 let burger = document.querySelector('.menu_burger')
 let navTablet = document.querySelector('.nav_tablet')
-burger.addEventListener("click", function open() {
+function openTabletMenu() {
   navTablet.classList.add("open");
+  burger.setAttribute("aria-expanded", "true");
+}
+
+function closeTabletMenu() {
+  navTablet.classList.remove("open");
+  navTablet.classList.add("navTablet");
+  burger.setAttribute("aria-expanded", "false");
+}
+
+burger.addEventListener("click", function open() {
+  openTabletMenu();
+});
+burger.addEventListener("keydown", function openWithKeyboard(event) {
+  if (event.key === "Enter" || event.key === " ") {
+    event.preventDefault();
+    openTabletMenu();
+  }
+  if (event.key === "Escape") {
+    closeTabletMenu();
+  }
 });
 
 navTablet.addEventListener("mouseout", function close() {
-   navTablet.classList.remove("open");
-   navTablet.classList.add("navTablet");
+   closeTabletMenu();
 });
--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -3,136 +3,59 @@ let textAnimal = document.querySelectorAll(".text_animal");
 let hText = document.querySelectorAll(".h6_text");
 let animalSmall = document.querySelectorAll(".animal_small")
 
-animal[0].addEventListener("mouseover", function hov() {
-  textAnimal[0].classList.add("hovered");
-  hText[0].classList.add("hovered");
-});
-animal[0].addEventListener("mouseout", function hove() {
-  textAnimal[0].classList.remove("hovered");
-  hText[0].classList.remove("hovered");
-});
-animal[1].addEventListener("mouseover", function hov() {
-  textAnimal[1].classList.add("hovered");
-  hText[1].classList.add("hovered");
-});
-animal[1].addEventListener("mouseout", function hove() {
-  textAnimal[1].classList.remove("hovered");
-  hText[1].classList.remove("hovered");
-});
-animal[2].addEventListener("mouseover", function hov() {
-  textAnimal[2].classList.add("hovered");
-  hText[2].classList.add("hovered");
-});
-animal[2].addEventListener("mouseout", function hove() {
-  textAnimal[2].classList.remove("hovered");
-  hText[2].classList.remove("hovered");
-});
-animal[3].addEventListener("mouseover", function hov() {
-  textAnimal[3].classList.add("hovered");
-  hText[3].classList.add("hovered");
-});
-animal[3].addEventListener("mouseout", function hove() {
-  textAnimal[3].classList.remove("hovered");
-  hText[3].classList.remove("hovered");
-});
-animal[4].addEventListener("mouseover", function hov() {
-  textAnimal[4].classList.add("hovered");
-  hText[4].classList.add("hovered");
-});
-animal[4].addEventListener("mouseout", function hove() {
-  textAnimal[4].classList.remove("hovered");
-  hText[4].classList.remove("hovered");
-});
-animal[5].addEventListener("mouseover", function hov() {
-  textAnimal[5].classList.add("hovered");
-  hText[5].classList.add("hovered");
-});
-animal[5].addEventListener("mouseout", function hove() {
-  textAnimal[5].classList.remove("hovered");
-  hText[5].classList.remove("hovered");
+animal.forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+    textAnimal[index].classList.add("hovered");
+    hText[index].classList.add("hovered");
+  });
+
+  item.addEventListener("mouseout", () => {
+    textAnimal[index].classList.remove("hovered");
+    hText[index].classList.remove("hovered");
+  });
 });
 
 
 
-animalSmall[0].addEventListener("mouseover", function hov() {
-  textAnimal[0].classList.add("hovered");
-  hText[0].classList.add("hovered");
-});
-animalSmall[0].addEventListener("mouseout", function hove() {
-  textAnimal[0].classList.remove("hovered");
-  hText[0].classList.remove("hovered");
-});
-animalSmall[1].addEventListener("mouseover", function hov() {
-  textAnimal[1].classList.add("hovered");
-  hText[1].classList.add("hovered");
-});
-animalSmall[1].addEventListener("mouseout", function hove() {
-  textAnimal[1].classList.remove("hovered");
-  hText[1].classList.remove("hovered");
-});
-animalSmall[2].addEventListener("mouseover", function hov() {
-  textAnimal[2].classList.add("hovered");
-  hText[2].classList.add("hovered");
-});
-animalSmall[2].addEventListener("mouseout", function hove() {
-  textAnimal[2].classList.remove("hovered");
-  hText[2].classList.remove("hovered");
-});
-animalSmall[3].addEventListener("mouseover", function hov() {
-  textAnimal[3].classList.add("hovered");
-  hText[3].classList.add("hovered");
-});
-animalSmall[3].addEventListener("mouseout", function hove() {
-  textAnimal[3].classList.remove("hovered");
-  hText[3].classList.remove("hovered");
-});
-animalSmall[4].addEventListener("mouseover", function hov() {
-  textAnimal[4].classList.add("hovered");
-  hText[4].classList.add("hovered");
-});
-animalSmall[4].addEventListener("mouseout", function hove() {
-  textAnimal[4].classList.remove("hovered");
-  hText[4].classList.remove("hovered");
-});
-animalSmall[5].addEventListener("mouseover", function hov() {
-  textAnimal[5].classList.add("hovered");
-  hText[5].classList.add("hovered");
-});
-animalSmall[5].addEventListener("mouseout", function hove() {
-  textAnimal[5].classList.remove("hovered");
-  hText[5].classList.remove("hovered");
+animalSmall.forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+    textAnimal[index].classList.add("hovered");
+    hText[index].classList.add("hovered");
+  });
+
+  item.addEventListener("mouseout", () => {
+    textAnimal[index].classList.remove("hovered");
+    hText[index].classList.remove("hovered");
+  });
 });
 
 
 let burger = document.querySelector('.menu_burger')
 let navTablet = document.querySelector('.nav_tablet')
-burger.addEventListener("click", function open() {
+function openTabletMenu() {
   navTablet.classList.add("open");
+  burger.setAttribute("aria-expanded", "true");
+}
+
+function closeTabletMenu() {
+  navTablet.classList.remove("open");
+  navTablet.classList.add("navTablet");
+  burger.setAttribute("aria-expanded", "false");
+}
+
+burger.addEventListener("click", function open() {
+  openTabletMenu();
+});
+burger.addEventListener("keydown", function openWithKeyboard(event) {
+  if (event.key === "Enter" || event.key === " ") {
+    event.preventDefault();
+    openTabletMenu();
+  }
+  if (event.key === "Escape") {
+    closeTabletMenu();
+  }
 });
 
 navTablet.addEventListener("mouseout", function close() {
-   navTablet.classList.remove("open");
-   navTablet.classList.add("navTablet");
+   closeTabletMenu();
 });
-
-
-
-
-const memoizedAdd = () => {
-  let cache = {};
-  return (9) => {
-    if (9 in cache) {
-      console.log('Fetching from cache');
-      return cache[9];
-    }
-    else {
-      console.log('Calculating result');
-      let result = 9 + 10;
-      cache[9] = result;
-      return result;
-    }
-  }
-  console.log(cache)
-
-}
-memoizedAdd()
